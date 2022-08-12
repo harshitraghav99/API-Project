b949c4f7414c6d96311d033e6180b9e5430640a1
harshit <harshitraghav8055@gmail.com>
2022-08-12 15:46:37 +0530

    init

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express=require("express");
+require("./database/connection")
+const Student=require("./models/students")
+const app=express();
+const port=process.env.PORT || 3000;
+app.use(express.json());
+// app.get("/",(req,res)=>{
+//     console.log("root");
+//     res.send("root");
+// })
+
+// create a new Student
+
+app.post("/students",(req,res)=>{
+    // res.send("post request");
+    console.log(req.body);
+    const student = new Student(req.body);
+    student.save().then(()=>{
+        res.status(201).send(student)
+    }).catch((e)=>{
+        
+        res.status(400).send(e);
+    });
+   
+})
+app.listen(port,()=>{
+    console.log(`connection on port ${port}`);
+})
