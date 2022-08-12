74f1cf49d633b5302d1e73ad3ee51d5666898212
harshit <harshitraghav8055@gmail.com>
2022-08-12 16:14:23 +0530

    post withasync/await

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-const express=require("express");
+const express = require("express");
 require("./database/connection")
-const Student=require("./models/students")
-const app=express();
-const port=process.env.PORT || 3000;
+const Student = require("./models/students")
+const app = express();
+const port = process.env.PORT || 3000;
 app.use(express.json());
 // app.get("/",(req,res)=>{
 //     console.log("root");
@@ -11,18 +11,27 @@ app.use(express.json());
 
 // create a new Student
 
-app.post("/students",(req,res)=>{
-    // res.send("post request");
-    console.log(req.body);
-    const student = new Student(req.body);
-    student.save().then(()=>{
-        res.status(201).send(student)
-    }).catch((e)=>{
-        
+// app.post("/students", (req, res) => {
+//     // res.send("post request");
+//     console.log(req.body);
+//     const student = new Student(req.body);
+//     student.save().then(() => {
+//         res.status(201).send(student)
+//     }).catch((e) => {
+
+//         res.status(400).send(e);
+//     });
+// })
+app.post("/students", async (req, res) => {
+    try {
+        const student = new Student(req.body);
+        const createUser = await student.save();
+        res.status(201).send(createUser);
+    } catch (e) {
         res.status(400).send(e);
-    });
-   
+    }
+
 })
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`connection on port ${port}`);
 })
